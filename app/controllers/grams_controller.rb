750bfe1b7587b09876dc6662986997bc6e2423ab
Nicholas Fearns <you@example.com>
2019-05-07 21:26:20 +0000

    generated a grams controller. created first tests. configured index page and created a view file for the index

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,7 @@
+class GramsController < ApplicationController
+
+  def index
+  end
+
+  
+end
---
+++ spec/controllers/grams_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe GramsController, type: :controller do
+  describe "grams#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+
+    end
+
+  end
+
+end
