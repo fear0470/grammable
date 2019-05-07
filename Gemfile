145b1e97d529a67526e9864772650ad4e43463b4
Nicholas Fearns <you@example.com>
2019-05-07 22:03:29 +0000

    generated gram model, added simpleform gem, created view file for new gram

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,7 +1,24 @@
 class GramsController < ApplicationController
 
+  def new
+    @gram = Gram.new
+  end
+
   def index
   end
 
-  
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+
+
 end
