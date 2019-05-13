3723567cf5097c5ca4f358ed65cdd13b4fa03574
Nicholas Fearns <you@example.com>
2019-05-13 23:13:02 +0000

    encoutering Frozen errors when testing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  
+  def create
+    @gram = Gram.find_by_id(params[:gram_id])
+    @gram.comments.creat(comment_params.merge(user: current_user))
+    redirect_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :grams
+  has_many :comments
    
 
 end
