32e31f407bb0b45073bfe44d43386ef40191bb1d
Nicholas Fearns <you@example.com>
2019-05-13 23:17:46 +0000

    encoutering Frozen errors when testing

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -57,9 +57,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
+  
 
 
 
